43059d805fc06009976edee2f4f2c6297517d2b2
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-02-11 05:09:22 -0300

    index, header y footer refactorizado

---
+++ controllers/indexController.js
@@ -0,0 +1,66 @@
+module.exports={
+    
+    index :  (req,res)=>{
+        res.render('index')
+    },
+    novedades :  (req,res)=>{
+        res.render('novedades')
+    },
+detalledecompra:(req,res)=>{
+res.render("detallesdecompra",{
+
+    
+})
+},
+detallesdelproducto:(req,res)=>{
+    res.render("detallesdeproducto",{
+    
+        
+    
+})
+},
+login:(req,res)=>{
+    res.render("login",{
+
+    })
+},
+registro:(req,res)=>{
+    res.render("registro",{
+
+    })
+},
+agregarproducto:(req,res)=>{
+    res.render("agregarproducto",{
+
+    })
+},
+/*index*/
+cartadelivery:(req,res)=>{
+    res.render("carta",{
+
+    })
+},
+locales:(req,res)=>{
+    res.render("locales",{
+
+    })
+},
+
+reservas:(req,res)=>{
+    res.render("reservas",{
+
+    })
+},
+
+
+promociones:(req,res)=>{
+    res.render("promociones",{
+
+    })
+},
+novedades:(req,res)=>{
+    res.render("novedades",{
+
+    })
+}
+}
